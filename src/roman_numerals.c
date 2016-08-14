cb03adacacdd5fdec00eab1be6c325d8113083ba
daveikaye <daveikaye@yahoo.com>
2016-08-14 19:03:28 -0400

    Re-factoring to_arabic

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -25,24 +25,34 @@ int roman_letter_to_arabic_number(char roman_letter) {
 
 }
 
+int roman_digit_to_arabic(char *roman, int* i_ptr, int roman_length) {
+    int number = roman_letter_to_arabic_number(roman[*i_ptr]);
+    (*i_ptr)++;
+
+    if (*i_ptr < roman_length) {
+        int nextNumber = roman_letter_to_arabic_number(roman[*i_ptr]);
+        if (nextNumber > number) {
+            number = nextNumber - number;
+            (*i_ptr)++;
+        }
+    }
+
+    return number;
+}
+
 int to_arabic(char *roman)
 {
+    if (roman == NULL) {
+
+        return -1;
+    }
+
     int i = 0;
     int arabic = 0;
     int roman_length = strlen(roman);
-
     while(i < roman_length) {
-        int number = roman_letter_to_arabic_number(roman[i]);
-        i++;
-
-        int nextNumber = roman_letter_to_arabic_number(roman[i]);
-        if (nextNumber > number) {
-            number = nextNumber - number;
-            i++;
-        }
-
-        arabic += number;
+        arabic += roman_digit_to_arabic(roman, &i, roman_length);
     }
 
-    return arabic;
+    return arabic <= 3999 && arabic > 0 ? arabic : -1;
 }
