37414d923744f875a26306986e855ae00f15de7c
daveikaye <daveikaye@yahoo.com>
2016-09-15 00:29:23 -0400

    Renaming add_or_subtract method to show that it adheres to SRP.

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -4,7 +4,7 @@
 #include "to_roman_converter.h"
 #include "to_arabic_converter.h"
 
-static char* add_or_subtract(char* roman1, char* roman2, bool should_add) {
+static char* do_arithmetic(char* roman1, char* roman2, bool should_add) {
     int arabic1 = to_arabic(roman1);
     int arabic2 = to_arabic(roman2);
 
@@ -20,11 +20,11 @@ static char* add_or_subtract(char* roman1, char* roman2, bool should_add) {
 char* add(char* roman1, char* roman2) {
     const bool should_add = true;
 
-    return add_or_subtract(roman1, roman2, should_add);
+    return do_arithmetic(roman1, roman2, should_add);
 }
 
 char* subtract(char* roman1, char* roman2) {
     const bool should_add = false;
 
-    return add_or_subtract(roman1, roman2, should_add);
+    return do_arithmetic(roman1, roman2, should_add);
 }
