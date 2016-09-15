c125202dec5c56e1eba6de7fda7ba5f68a928bbc
daveikaye <daveikaye@yahoo.com>
2016-09-14 23:55:13 -0400

    Use stdbool.h for bool instead of int as a bool flag.

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -1,9 +1,10 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "to_roman_converter.h"
 #include "to_arabic_converter.h"
 
-static char* add_or_subtract(char* roman1, char* roman2, int should_add) {
+static char* add_or_subtract(char* roman1, char* roman2, bool should_add) {
     int arabic1 = to_arabic(roman1);
     int arabic2 = to_arabic(roman2);
 
@@ -17,13 +18,13 @@ static char* add_or_subtract(char* roman1, char* roman2, int should_add) {
 }
 
 char* add(char* roman1, char* roman2) {
-    const int should_add = 1;
+    const bool should_add = true;
 
     return add_or_subtract(roman1, roman2, should_add);
 }
 
 char* subtract(char* roman1, char* roman2) {
-    const int should_add = 0;
+    const bool should_add = false;
 
     return add_or_subtract(roman1, roman2, should_add);
 }
