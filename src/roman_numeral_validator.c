bb6c5dc55b5fec937817933d9d48b9ef40022a9c
daveikaye <daveikaye@yahoo.com>
2016-09-26 19:48:35 -0400

    Moving roman numeral validation into the validation module.

--- src/roman_numeral_validator.c
+++ src/roman_numeral_validator.c
@@ -1,9 +1,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "roman_letter_to_arabic_number_converter.h"
 
 static bool basic_roman_numeral_validation_passed(char* roman) {
     if (roman == NULL) {
+
+        return false;
+    }
+
+    return true;
+}
+
+static bool is_valid_roman_letter(char roman_letter) {
+    int arabic = roman_letter_to_arabic_number(roman_letter);
+    if (arabic == -1) {
 
         return false;
     }
@@ -26,6 +37,11 @@ bool is_roman_numeral_valid(char *roman) {
     int i = 0;
     int iCounter = 0;
     while(i < roman_length) {
+        if (!is_valid_roman_letter(roman[i])) {
+
+            return false;
+        }
+
         if (roman[i] == 'I') {
             iCounter++;
             if(iCounter == 4) {
