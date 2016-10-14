5c942ce38051ab37333affd8ed24a9bb4ee5b13d
daveikaye <daveikaye@yahoo.com>
2016-10-13 22:38:46 -0400

    Re-factoring do_addition

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -1,15 +1,22 @@
 #include <string.h>
+#include <stdbool.h>
 #include "to_roman_converter.h"
 #include "to_arabic_converter.h"
 
 const int POSITIVE_MULTIPLIER = 1;
 const int NEGATIVE_MULTIPLIER = -1;
+const int MIN_ARABIC_VALUE = 1;
+
+static bool operands_valid(int operand1, int operand2) {
+
+    return operand1 >= MIN_ARABIC_VALUE && operand2 >= MIN_ARABIC_VALUE;
+}
 
 static char* do_addition(char* maxMemoryBuffer, char* roman1, char* roman2, int signMultiplier) {
     int arabic1 = to_arabic(maxMemoryBuffer, roman1);
     int arabic2 = to_arabic(maxMemoryBuffer, roman2);
 
-    if (arabic1 > 0 && arabic2 > 0) {
+    if (operands_valid(arabic1, arabic2)) {
         int arabic_result = arabic1 + (arabic2 * signMultiplier);
 
         return to_roman(maxMemoryBuffer, arabic_result);
